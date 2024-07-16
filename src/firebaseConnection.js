22acb21ad846d53257c905bbc5cf2d3bcd3fd880
Luis Fernando <luisfernando.mourag@gmail.com>
2024-07-16 17:10:30 -0300

    add environment variables

--- src/firebaseConnection.js
+++ src/firebaseConnection.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmI7WlBxZ5Bo-KwwaEo_t2_9iJkXuuq-I",
-  authDomain: "curso-60b53.firebaseapp.com",
-  projectId: "curso-60b53",
-  storageBucket: "curso-60b53.appspot.com",
-  messagingSenderId: "586187349682",
-  appId: "1:586187349682:web:9d09013ff0108764d3b3ab",
-  measurementId: "G-JB0YJDVNT5",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize
